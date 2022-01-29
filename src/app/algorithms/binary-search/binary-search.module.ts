bd7502af981c68bceef54f2789114d3d409cb7ad
CHRIS NGUYEN <chrisnguyen@chriss-mbp.lan>
2022-01-29 06:39:03 -0800

    Included binary-search component for learning

---
+++ src/app/algorithms/binary-search/binary-search.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BinarySearchComponent } from './binary-search.component';
+import { BinarySearchRoutingModule } from './binary-search-routing.module';
+
+@NgModule({
+  declarations: [BinarySearchComponent],
+  imports: [
+    CommonModule,
+    BinarySearchRoutingModule
+  ],
+  exports: [
+    BinarySearchComponent
+  ]
+})
+export class BinarySearchModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
